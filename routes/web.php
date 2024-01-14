35a6ef6796f7407f6165ce318051d71e4f161249
RicDev116 <ricardo_29leyva@outlook.com>
2024-01-14 00:59:22 -0600

    1-1 configurar payout

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 //group
 Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function(){
+
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
